7d36d10311865cda83f443142d31577df56d35e3
FlexinUp <60309475+FlexinUp@users.noreply.github.com>
2024-01-31 20:02:04 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Ban'
+
+
+ui_page 'web/index.html'
+files{
+    'web/index.html',
+    'web/css/*.css',
+    'web/assets/logo.png'
+}
+
+escrow_ignore {
+    'css/logo.css',
+    'css/styles.css'
+}
